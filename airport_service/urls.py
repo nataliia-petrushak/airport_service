7682d4040a7956a63002f611661f4f806bd8d0b1
Amirammi <amirammina@gmail.com>
2023-09-06 14:57:24 +0300

    Initialized user app

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
